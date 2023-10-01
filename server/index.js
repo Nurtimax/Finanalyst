b25ec7e2df1627a8f20cc720b1fc1db9235f1726
Nurtimax <maksatbeknurtilek@gmail.com>
2023-10-01 19:38:41 +0600

    create telegram bot

---
+++ server/index.js
@@ -0,0 +1,46 @@
+import TelegramBot from 'node-telegram-bot-api';
+import 'dotenv/config';
+
+const token =
+  // eslint-disable-next-line no-undef
+  process.env.REACT_APP_TOKEN;
+
+const webAppUrl = 'http://192.168.251.15:3000';
+
+if (token) {
+  const bot = new TelegramBot(token, { polling: true });
+
+  bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text === '/start') {
+      return await bot.sendMessage(chatId, 'Welcome my channels /end');
+    }
+
+    if (text === '/end') {
+      return await bot.sendMessage(chatId, 'Goodbye my channels /start /register');
+    }
+
+    if (text === '/register') {
+      return await bot.sendMessage(chatId, 'Have button and enter your forms', {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: 'Enter delivery',
+                web_app: { url: webAppUrl }
+              }
+            ]
+          ]
+        }
+      });
+    }
+
+    try {
+      await bot.sendMessage(chatId, 'Received your message');
+    } catch (error) {
+      console.error(error);
+    }
+  });
+}
